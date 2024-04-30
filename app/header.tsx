0fdc5eecff8d6f7e33b479ae00c1093d3c905a57
Brandon Davis <git@subdavis.com>
2024-04-30 12:22:45 -0500

    Simple header update

--- app/header.tsx
+++ app/header.tsx
@@ -1,24 +1,29 @@
 function inlineBlockWord(word: string) {
-  return <span className="block">
-    <span className="inline rounded bg-stone-950 text-stone-100 px-2">{word}&nbsp;</span>
+  return <span className="inline-block sm:block mr-2">
+    <span className="inline rounded bg-stone-950 text-stone-100 px-2">{word}</span>
   </span>
 }
 
 function bigwheel() {
-  return <div className="relative">
-    <div className="absolute -z-10 bg-[url('/wheel.small.webp')] w-[512px] h-[512px] bg-contain animate-spin-slow -top-[536px] -right-16" />
+  return <div className="relative -mt-[420px] -ml-14 sm:ml-0 sm:mt-0">
+    <div className="
+      -z-10 bg-[url('/wheel.small.webp')] w-[512px] h-[512px] bg-contain
+      sm:absolute sm:animate-spin-slow sm:-top-[286px] sm:-right-16" />
   </div>
 }
 
 export default function Header() {
   return <header>
+    {bigwheel()}
     <h1 className="z-10 text-4xl font-semibold mb-5">
       {inlineBlockWord('2024')}
+      <span> </span>
       {inlineBlockWord('Minneapolis')}
+      <span> </span>
       {inlineBlockWord('Bike')}
+      <span> </span>
       {inlineBlockWord('Clubs')}
     </h1>
-    {bigwheel()}
     <p className="my-3 z-10 bg-white">
       Join a group bicycle ride with a community of cyclists in Minneapolis.
       This is a list of active teams and clubs that host rides throughout the season.
